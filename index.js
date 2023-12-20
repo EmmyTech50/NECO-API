b33a5ba23577be6d1c900c96333209bb13e29a86
EmmyTech50 <emmagospelnews@gmail.com>
2023-12-20 05:24:59 -0800

    changes

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
+
 
 const app = express();
 const PORT = 5000;
+
+
+// add middleware
+app.use(bodyParser.json());
+
+//get the mongoose coonection
+mongoose.connect('mongodb://localhost:27017/neco');
+const conn = mongoose.connection;
 
 // create an endpoint
 app.get('/', (req, res) => {
@@ -10,7 +22,32 @@ app.get('/', (req, res) => {
 
 
 // listen to the port for any connection
-app.post('/hello', (req, res) => {
+app.post('/create', (req, res) => {
+   const body = req.body; 
+   const { email } = body;
+
+    // store the user information to the database
+    // create response body
+   const userSuccessRes = { 
+                    status: 'success',
+                    message:`User registration for ${email} is successful `
+                    };
+    // send back a response message
+   if(body) res.send(userSuccessRes);
+
+   
+   
+})
+
+app.post('/login', (req, res) => {
+   const data = [
+        { name: 'Anae', age: 40},
+        { name: 'markus', age: 45},
+   ];
+   res.send();
+})
+
+app.post('/forgetPassword', (req, res) => {
    const data = [
         { name: 'Anae', age: 40},
         { name: 'markus', age: 45},
@@ -28,3 +65,9 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`NecoApp listening on port ${PORT}`)
 })
+
+
+// createAccount, login, forgetPassword,
+
+
+//CRUD : Create Read Update Delete
